8f838c9be7f2a6f1186b77db073c1741303cdada
shivonnu <shivonnu@gmail.com>
2020-12-09 09:10:13 +0900

    ユーザー登録画面の作成

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -6,7 +6,7 @@
   <div class="container">
     <div class="row">
       <div class="mx-auto col col-12 col-sm-11 col-md-9 col-lg-7 col-xl-6">
-        <h1 class="text-center"><a class="text-dark" href="/">memo</a></h1>
+        <h1 class="text-center"><a class="text-dark" href="/">b-girl-sns</a></h1>
         <div class="card mt-3">
           <div class="card-body text-center">
             <h2 class="h3 card-title text-center mt-2">ユーザー登録</h2>
@@ -20,14 +20,18 @@
               <small>英数字３〜１６文字(登録後の変更はできません)</small>
             </div>
             <div class="md-form">
-              <label dor="email">メールアドレス</label>
-              <input class="form-control" type="text" id="email" name="email" required value="{{ old('email) }}">
+              <label for="email">メールアドレス</label>
+              <input class="form-control" type="text" id="email" name="email" required value="{{ old('email') }}" >
             </div>
             <div class="md-form">
+            <label for="password">パスワード</label>
+                  <input class="form-control" type="password" id="password" name="password" required>
+                </div>
+                <div class="md-form">
               <label for="password_confirmation">パスワード(確認)</label>
               <input class="form-control" type="password" id="password_confirmation" name="password_confirmation" required>
             </div>
-            <button clas="btn btn-blok blur-gradient mt-2 mb-2" type="submit">ユーザー登録</button>
+            <button class="btn btn-block  #fce4ec pink lighten-5 mt-2 mb-2" type="submit">ユーザー登録</button>
             </form>
 
             <div class="mt-0">
