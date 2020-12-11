291d85a6d31f82a243b8ecdc15127baf7d23d805
shivonnu <shivonnu@gmail.com>
2020-12-11 12:23:21 +0900

    ハートアニメーション機能の実装
    bootstrapでアニメーション実装動作確認しました。

--- resources/js/components/ArticleLike.vue
+++ resources/js/components/ArticleLike.vue
@@ -5,7 +5,7 @@
       class="btn m-0 p-1 shadow-none"
     >
       <i class="fas fa-heart mr-1"
-      :class="{'red-text':this.isLikedBy}" 
+      :class="{'red-text':this.isLikedBy, 'animated heartBeat fast':this.gotToLike}" 
       @click="clickLike" 
       />
     </button>
@@ -37,6 +37,7 @@ export default {
       return {
         isLikedBy: this.initialIsLikedBy,
         countLikes: this.initialCountLikes,
+        gotToLike: false,
       }
     },
     methods: {
@@ -55,12 +56,14 @@ export default {
 
         this.isLikedBy = true
         this.countLikes = response.data.countLikes
+        this.gotToLike = true 
       },
       async unlike() {
         const response = await axios.delete(this.endpoint)
 
         this.isLikedBy = false
         this.countLikes = response.data.countLikes
+        this.gotToLike = false
       },
     },
   }
