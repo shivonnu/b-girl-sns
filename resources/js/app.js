c84665894e55132a7565ef3d39aa2ee2d81072f0
shivonnu <shivonnu@gmail.com>
2020-12-11 12:38:01 +0900

    タグ入力フォーム作成
    タグ入力フォームの作成

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,7 @@
 import './bootstrap'
 import Vue from 'vue'
 import ArticleLike from './components/ArticleLike'
-import ArticleTagsInput from './components/ArticleTagsInput
+import ArticleTagsInput from './components/ArticleTagsInput'
 
 const app = new Vue({
   el: '#app',
