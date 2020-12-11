59c1fb06cbace703afbe9260335c48ebfc860782
shivonnu <shivonnu@gmail.com>
2020-12-11 20:21:29 +0900

    タグごとの一覧機能を作る
    タグごとの一覧機能を作る

--- app/Tag.php
+++ app/Tag.php
@@ -3,6 +3,8 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+
 
 class Tag extends Model
 {
@@ -13,6 +15,11 @@ class Tag extends Model
     public function getHashtagAttribute(): string
     {
         return '#' . $this->name;
+    }
+
+    public function articles(): BelongsToMany
+    {
+        return $this->belongsToMany('App\Article')->withTimestamps();
     }
 
 }
